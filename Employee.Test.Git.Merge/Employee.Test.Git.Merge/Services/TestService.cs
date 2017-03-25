112deb515b3fd3ab610748c4a99c43041504aefe
Mario Perić <pericmario96@gmail.com>
2017-03-25 17:49:19 +0100

    commit

--- Employee.Test.Git.Merge/Employee.Test.Git.Merge/Services/TestService.cs
+++ Employee.Test.Git.Merge/Employee.Test.Git.Merge/Services/TestService.cs
@@ -17,15 +17,13 @@ namespace Employee.Test.Git.Merge.Services
 
         public List<string> PeopleNames()
         {
-<<<<<<< HEAD
+
             var peopleNames = _testRepository.GetPeopleNames();
 
             List<string> noShortFirstNames = peopleNames.Where(name => name.Split(' ')[0].Length > 3).ToList();
+           
 
-            return noShortFirstNames;
-=======
-            return _testRepository.GetPeopleNames().Select(name => name.ToUpper()).ToList();
->>>>>>> ff1d588a1fa537d458ea570c57e65a491689f69d
+            return noShortFirstNames.Select(name => name.ToUpper()).ToList();
         }
     }
 }
