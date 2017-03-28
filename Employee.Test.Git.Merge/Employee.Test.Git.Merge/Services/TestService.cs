35d4978da62c510b796042c446405a769421f999
Straja12 <prefiks12@gmail.com>
2017-03-28 10:48:01 +0200

    ispravka v4

--- Employee.Test.Git.Merge/Employee.Test.Git.Merge/Services/TestService.cs
+++ Employee.Test.Git.Merge/Employee.Test.Git.Merge/Services/TestService.cs
@@ -17,7 +17,10 @@ namespace Employee.Test.Git.Merge.Services
 
         public List<string> PeopleNames()
         {
-            return _testRepository.GetPeopleNames();
+
+             return _testRepository.GetPeopleNames().Select(name => name.ToUpper()).ToList();
+         
+
         }
     }
 }
