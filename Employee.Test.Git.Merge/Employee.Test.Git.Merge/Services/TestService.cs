8f1a48541865cc963c96ae1f196751907ea8ba0e
slomil12345 <ppera9963@gmail.com>
2017-03-28 10:39:02 +0200

    Update TestService.cs

--- Employee.Test.Git.Merge/Employee.Test.Git.Merge/Services/TestService.cs
+++ Employee.Test.Git.Merge/Employee.Test.Git.Merge/Services/TestService.cs
@@ -17,6 +17,7 @@ namespace Employee.Test.Git.Merge.Services
 
         public List<string> PeopleNames()
         {
+            var peopleNames=_testRepository.GetPeopleNames().Select(name => name.ToUpper());
             return _testRepository.GetPeopleNames().Select(name => name.ToUpper()).ToList();
         }
     }
