536a102b4a215749cef8ece39565e17452e3a0fc
Stefanos <dr_ja_sl@hotmail.com>
2017-03-29 12:56:46 +0200

    Merge successfull

--- Employee.Test.Git.Merge/Employee.Test.Git.Merge/Services/TestService.cs
+++ Employee.Test.Git.Merge/Employee.Test.Git.Merge/Services/TestService.cs
@@ -17,9 +17,9 @@ namespace Employee.Test.Git.Merge.Services
 
         public List<string> PeopleNames()
         {
-            var peopleNames = _testRepository.GetPeopleNames();
+            var peopleNames = _testRepository.GetPeopleNames().Select(name => name.ToUpper()).ToList();
 
-            List<string> noShortFirstNames = peopleNames.Where(name => name.Split(' ')[0].Length > 3).ToList();
+            List<string> noShortFirstNames = peopleNames.Where(name => name.Split(' ')[0].Length > 4).ToList();
 
             return noShortFirstNames;
         }
