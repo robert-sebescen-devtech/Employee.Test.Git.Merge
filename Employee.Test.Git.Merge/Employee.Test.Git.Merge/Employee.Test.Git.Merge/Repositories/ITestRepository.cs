c3012fa0163c61718ddae1ffe2a4ddd4ff26f925
NikolaDjokic94 <Djo_94@live.com>
2017-03-29 18:48:02 +0200

    pull Master

---
+++ Employee.Test.Git.Merge/Employee.Test.Git.Merge/Employee.Test.Git.Merge/Repositories/ITestRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee.Test.Git.Merge.Repositories
+{
+    public interface ITestRepository
+    {
+        List<string> GetPeopleNames();
+    }
+}
---
+++ Employee.Test.Git.Merge/Employee.Test.Git.Merge/Employee.Test.Git.Merge/Services/TestService.cs
@@ -0,0 +1,30 @@
+﻿using Employee.Test.Git.Merge.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Employee.Test.Git.Merge.Services
+{
+    public class TestService : ITestService
+    {
+        private readonly ITestRepository _testRepository;
+
+        public TestService(ITestRepository testRepository)
+        {
+            _testRepository = testRepository;
+        }
+
+        public List<string> PeopleNames()
+        {
+
+            var peopleNames = _testRepository.GetPeopleNames();
+
+            List<string> noShortFirstNames = peopleNames.Where(name => name.Split(' ')[0].Length > 3).ToList();
+
+            return noShortFirstNames;
+
+            return _testRepository.GetPeopleNames().Select(name => name.ToUpper()).ToList();
+        }
+    }
+}
