a598a773fe0bab7b2865ca6d3e683a6ffb0da47b
Miloš Kajtez <kajtez@gmail.com>
2017-03-29 15:07:27 +0200

    Update TestService.cs

--- Employee.Test.Git.Merge/Employee.Test.Git.Merge/Services/TestService.cs
+++ Employee.Test.Git.Merge/Employee.Test.Git.Merge/Services/TestService.cs
@@ -17,11 +17,10 @@ namespace Employee.Test.Git.Merge.Services
 
         public List<string> PeopleNames()
         {
-            var peopleNames = _testRepository.GetPeopleNames()
+            var peopleNames = _testRepository.GetPeopleNames();
             List<string> noShortFirstNames = peopleNames.Where(name => name.Split(' ')[0].Length > 3).ToList();
- 		             
-             	             
-            return noShortFirstNames.Select(name => name.ToUpper()).ToList()
+ 		    
+            return noShortFirstNames.Select(name => name.ToUpper()).ToList();
 
         }
     }
