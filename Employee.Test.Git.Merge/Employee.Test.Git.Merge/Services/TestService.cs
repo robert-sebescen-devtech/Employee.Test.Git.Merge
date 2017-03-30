ddf4163644c503f6ed267f46485e4e42764d967a
bukela <bukela@gmail.com>
2017-03-30 23:11:07 +0200

    Update TestService.cs

--- Employee.Test.Git.Merge/Employee.Test.Git.Merge/Services/TestService.cs
+++ Employee.Test.Git.Merge/Employee.Test.Git.Merge/Services/TestService.cs
@@ -18,7 +18,7 @@ namespace Employee.Test.Git.Merge.Services
         public List<string> PeopleNames()
         {
             var peopleNames = _testRepository.GetPeopleNames();
-
+            return _testRepository.GetPeopleNames().Select(name => name.ToUpper()).ToList();
             List<string> noShortFirstNames = peopleNames.Where(name => name.Split(' ')[0].Length > 3).ToList();
 
             return noShortFirstNames;
