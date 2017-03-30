c7c4ea649ac10cfa57b5a69b686e4b3804f17799
Stefan <dr_ja_sl@hotmail.com>
2017-03-30 13:04:07 +0200

    from 3 to 4

--- Employee.Test.Git.Merge/Employee.Test.Git.Merge/Services/TestService.cs
+++ Employee.Test.Git.Merge/Employee.Test.Git.Merge/Services/TestService.cs
@@ -22,7 +22,7 @@ namespace Employee.Test.Git.Merge.Services
 
             var peopleNames = _testRepository.GetPeopleNames();
 
-            List<string> noShortFirstNames = peopleNames.Where(name => name.Split(' ')[0].Length > 3).ToList();
+            List<string> noShortFirstNames = peopleNames.Where(name => name.Split(' ')[0].Length > 4).ToList();
 
             return noShortFirstNames;
 
